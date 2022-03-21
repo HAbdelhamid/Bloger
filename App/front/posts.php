f4751eaa535af15afca1e221865e5f1af5ac2532
hamza abdelhamid <abdelhamidhamzaa@gmail.com>
2022-03-21 21:36:46 +0100

    Add: Get data from database and use it on posts

--- App/front/posts.php
+++ App/front/posts.php
@@ -1,83 +1,40 @@
+<?php
+    require("App/back/db.php");
+?>
 <div class="p-4">
-    <!-- Card -->
-    <div class="card col m-4 col-md-3">
+    <?php
+        $sql = "SELECT * FROM posts";
+        $result = mysqli_query($conn, $sql);
+        while ($post=mysqli_fetch_assoc($result)) {
+    ?>
+        <!-- Card -->
+        <div class="card m-4 p-0 col-md-6 col-lg-4">
 
-        <!-- Card image -->
-        <div class="view overlay">
-        <img class="card-img-top " src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).webp"
-            alt="Card image cap">
-        <a href="#!">
-            <div class="mask rgba-white-slight"></div>
-        </a>
-        </div>
+            <!-- Card image -->
+            <div class="view overlay">
+            <img class="card-img-top" src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).webp"
+                alt="Card image cap">
+            <a href="#!">
+                <div class="mask rgba-white-slight"></div>
+            </a>
+            </div>
 
-        <!-- Card content -->
-        <div class="card-body">
+            <!-- Card content -->
+            <div class="card-body">
 
-        <!-- Title -->
-        <h4 class="card-title">Card title</h4>
-        <!-- Text -->
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
-        <!-- Button -->
-        <a href="#" class="btn btn-primary">Button</a>
+            <!-- Title -->
+            <h4 class="card-title"><?=$post['title']?></h4>
+            <!-- Text -->
+            <p class="card-text"><?=$post['content']?></p>
+            <!-- Button -->
+            <a href="#" class="btn btn-primary">Button</a>
 
-        </div>
-
-    </div>
-    <!-- Card -->
-    <!-- Card -->
-    <div class="card col m-4 col-md-3">
-
-        <!-- Card image -->
-        <div class="view overlay">
-        <img class="card-img-top" src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).webp"
-            alt="Card image cap">
-        <a href="#!">
-            <div class="mask rgba-white-slight"></div>
-        </a>
-        </div>
-
-        <!-- Card content -->
-        <div class="card-body">
-
-        <!-- Title -->
-        <h4 class="card-title">Card title</h4>
-        <!-- Text -->
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
-        <!-- Button -->
-        <a href="#" class="btn btn-primary">Button</a>
+            </div>
 
         </div>
+        <!-- Card -->
+    <?php    
+        }
+    ?>
 
-    </div>
-    <!-- Card -->
-    <!-- Card -->
-    <div class="card col m-4 col-md-3">
-
-        <!-- Card image -->
-        <div class="view overlay">
-        <img class="card-img-top" src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).webp"
-            alt="Card image cap">
-        <a href="#!">
-            <div class="mask rgba-white-slight"></div>
-        </a>
-        </div>
-
-        <!-- Card content -->
-        <div class="card-body">
-
-        <!-- Title -->
-        <h4 class="card-title">Card title</h4>
-        <!-- Text -->
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
-        <!-- Button -->
-        <a href="#" class="btn btn-primary">Button</a>
-
-        </div>
-
-    </div>
-    <!-- Card -->
 </div>
